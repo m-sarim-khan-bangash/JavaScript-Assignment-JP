07ced8b73ebd6e20f9dc76777d5ee37c792ee2a2
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2024-02-20 20:39:43 +0500

    JS Assignment

---
+++ PDF_1_Sol/app.js
@@ -0,0 +1,82 @@
+// *****************************************Chapter 01***********************************
+
+// Question #01
+alert("Hello, Welcome to my website")
+
+// Question #02
+alert("Error! Please Enter a Valid Password")
+
+// Question #03
+alert("Welcome to JS Land.. \nHappy Coding")
+
+// Question #04
+alert("Welcome to JS Land \nprevent this page for creating additional dialouges")
+alert("Happy Coding")
+
+// Question #05
+console.log("Hello i can run JS thorough my Browser's console")
+
+// **************************************Chapter 02******************************************
+
+// Question #01
+var username = "Muhammad Sarim"
+
+// Question #02
+var myName = "Sarim"
+
+console.log("My name is: ", myName)
+ = 15
+var course = "Certified Mob App Development"
+
+alert(name)
+alert(age, " years old")
+// Question #03
+var message = "Hello World"
+alert(message)
+
+// Question #04
+
+var name = "John Doe"
+var age
+
+// Question #05
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+
+
+// Question #06
+var email = "example@example.com"
+alert("My email address is: " + email)
+
+// Question #07
+var book  = "A Smarter Way to Learn JS"
+alert("I am trying to learn from a Book: " + book)
+
+// Question #08
+document.write("Yeah i can write on browser through JS!!")
+
+// Question #09
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design)
+
+// ***************************************Chapter 03******************************
+
+// Question #01
+ var age = 21
+ alert("I am " + age + " years old")
+
+// Question #02
+var visited = 14
+alert("You have visited " + visited + " times")
+
+// Question #03
+var birthYear = 2002
+document.write("My Birth Year is ", birthYear)
+document.write("Data Type of my Declared variable is: ", typeof birthYear)
+
+// Question #04
+var VisitorName = "Jhon Doe"
+var quantity = 5
+var Product = "T-Shirt"
+
+document.write("<b>",VisitorName, "</b>"," has ordered", " ","<b>", quantity,"</b>", " ","<b>", Product,"</b>", " from XYZ Clothing Store")
---
+++ PDF_2_Sol/app.js
@@ -0,0 +1,59 @@
+// *****************************Chapter #01*****************************
+
+// Question 01
+alert("Muhammad");
+alert("Sarim");
+alert("bangashsarim@gmail.com");
+alert("0333-1234567");
+alert("Password: ********");
+
+// Question 02
+alert("You are learning JavaScript");
+
+// Question 03
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// ********************************Chapter #02*****************************
+
+// Question 01
+var userName = "Muhammad Sarim";
+
+// Question 02
+var myName;
+myName = "Muhammad Sarim";
+
+// Question 03
+var teamName = "Peshawar Zalmi";
+alert(teamName);
+
+// Question 04
+
+var bestMan = "Charlie";
+alert(bestMan);
+
+// ********************************Chapter #03*****************************
+
+// Question 01
+var caseQty;
+
+// Question 02
+caseQty = 144;
+
+// Question 03
+var num = 9;
+
+// Question 04
+var sumResult;
+sumResult = 5 + 7;
+console.log(sumResult);
+
+// Question 05
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal); //110
+
+// Question 06
+var myNumber = 10;
+myNumber = myNumber + 5;
+console.log(myNumber);
